fdb9d99ff1b2ad946f4cca5636a243fba91a8e5b
mattyanthony <108240399+mattyanthony@users.noreply.github.com>
2024-02-06 21:38:40 -0500

    updates to App.jsx, created BaristaForm.jsx

---
+++ Barista-App/src/Components/BaristaForm.jsx
@@ -0,0 +1,39 @@
+import React, {Component, useState} from "react";
+
+const BaristaForm = () => {
+
+    const onNewDrink = () => {
+    
+    };
+    
+    const onCheckAnswer = () => {
+      
+    };
+  
+    return (
+        <div>
+        <form >
+            
+            // we will fill this in soon!
+            
+        </form>
+
+        <button type="submit" className="button submit" onClick={onCheckAnswer}>
+            Check Answer
+        </button>
+
+        <button
+            type="new-drink-button"
+            className="button newdrink"
+            onClick={onNewDrink}
+        >
+            New Drink
+        </button>
+        </div>
+    
+    )
+  
+};
+
+
+export default BaristaForm;
